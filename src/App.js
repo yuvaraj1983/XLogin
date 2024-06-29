e3a4db48dc657729409fb6d8e56bddda09406954
yuvaraj <yuvaraj@coursera-something.com>
2024-06-29 21:14:56 +0530

    login page completed

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword]= useState('');
+  const [welcomemsg, setWelcomeMsg]= useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(username !== 'user' && password!== 'password') {
+      setWelcomeMsg(`Invalid username or password`);
+      
+    } else {
+      setWelcomeMsg(`Welcome ${username}!`)
+    }
+   
+  }
+  return (
+    <div >
+      <h1>Login Page</h1>
+     <form onSubmit={handleSubmit}>
+      <label>Username</label>
+      <input required type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+      <br />
+      <label>Password</label>
+      <input required type='text' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button>Submit</button>
+      <br />
+      <div>{welcomemsg}</div>
+     </form>
+    </div>
+  );
+}
+
+export default App;
